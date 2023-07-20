8533d4c8136ec5e3578d028e6b858f7e7bee9587
SANJIB KUMAR DAS <sanjibdas.23012002@gmail.com>
2023-07-21 00:40:16 +0530

    update

---
+++ src/Prog9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Prog9 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner inp = new Scanner(System.in);
+        int num = inp.nextInt();
+        int ans = isPrime(num);
+        if(ans == num){
+            System.out.println("Armstrong number");
+        } else {
+            System.out.println("Not an armstrong number");
+        }
+    }
+    static int isPrime(int n){
+        int count=0, sum=0;
+        int temp=n;
+        while(temp>0){
+            temp=temp/10;
+            count++;
+        }
+        while(n>0){
+            int rem = n%10;
+            sum = (int) (sum + Math.pow(rem,count));
+            n=n/10;
+        }
+        return sum;
+    }
+}
